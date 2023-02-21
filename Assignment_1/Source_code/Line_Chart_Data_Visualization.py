f7cf833582aaff2099dfd8066bbdec622f5f5315
Bhaskar <cherukubhaskar3@gmail.com>
2023-02-21 10:41:45 -1200

    Added multiple line plots

--- Assignment_1/Source_code/Line_Chart_Data_Visualization.py
+++ Assignment_1/Source_code/Line_Chart_Data_Visualization.py
@@ -1,24 +1,31 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
-def ReadCSVData():
-    import pandas as pd
+def ReadData():
     return pd.read_excel('D:/WorkSpace/Applied_Data_Science/Assignment_1/Data_sets/covid_worldwide.xlsx',nrows=8)
     
 ''' Function definition'''
-def LicenCHart(): 
-   df = ReadCSVData()
-   x_axis= df['Country']
-   y_axis=df['Total Deaths'].astype(int)
-   plt.plot(x_axis,y_axis);
+def MultiplePlot(): 
+    
+   '''Get data'''
+   data = ReadData()
+   
+   ''' X axis Data '''
+   x_axis = data['Country'] 
+   
+   y_axis  = data['Total Cases']
+   y_axis1 = data['Total Deaths']
+   y_axis2 = data['Total Recovered']
+   y_axis3 = data['Active Cases']
+   
+   plt.plot(x_axis,y_axis,x_axis,y_axis1,x_axis,y_axis2,x_axis,y_axis3);
+   
    plt.ylabel("Count")
    plt.xlabel('Country')
    plt.title("COVID CASES")
-   plt.legend("Total Deaths")
-   ''' plt.set_ylim(100000,999999999,100000)'''
+   plt.legend(["Total Cases","Total Deaths","Total Recovered","Active Cases"])
    plt.show()
    return 
 
 '''Function calling'''
-LicenCHart()
+MultiplePlot()
