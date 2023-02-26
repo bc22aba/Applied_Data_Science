13368c6b6ff6faab867ada2ce75ddfb8c0244fde
Bhaskar <cherukubhaskar3@gmail.com>
2023-02-25 13:19:24 -1200

    Created Pie_Chart And Added appropriate Data_sets file

---
+++ Assignment_1/Source_code/Pie_Chart.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 25 12:44:38 2023
+
+@author: cheru
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+'''Data link: https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population'''
+'''Read Excel file Data using pandas'''
+def ReadData():
+    return pd.read_excel('D:/WorkSpace/Applied_Data_Science/Assignment_1/Data_sets/Pie_Chart_DataSet.xlsx')
+
+'''Function Definition to Generate Pie_Chart Plot'''
+def PieChart():
+    
+    '''Get Data from Excel file'''
+    population_data = ReadData()
+    data = population_data["Population Percentage"]
+    labels =population_data["Country Name"]
+    plt.pie(data,labels=labels,autopct='%1.1f%%');
+    plt.title("% of World Population")
+    plt.show()
+    return
+
+'''Function Calling to Generate Pie_Chart'''
+PieChart()
+
